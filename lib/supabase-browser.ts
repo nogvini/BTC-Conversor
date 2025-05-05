2dca35e9f4bfad352ff9c21dbee19d7730f7852a
nogvini <vinicius.nogueira@iesgo.edu.br>
2025-05-05 19:04:43 -0300

    coreção das variaveis de ambiente

--- lib/supabase-browser.ts
+++ lib/supabase-browser.ts
@@ -2,6 +2,12 @@
 
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 
-export const supabaseBrowser = createClientComponentClient()
+const supabaseUrl = 'https://sqnxrzndkppbwqdmvzer.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxbnhyem5ka3BwYndxZG12emVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0MDA0NDMsImV4cCI6MjA2MTk3NjQ0M30.yaMQFTEWoNT3OeOCq-P05w39hpe1ppDcMp4DR7gVMRw'
+
+export const supabaseBrowser = createClientComponentClient({
+  supabaseUrl,
+  supabaseKey,
+})
 
 export default supabaseBrowser 
