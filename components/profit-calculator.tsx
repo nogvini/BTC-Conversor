42295ae4795f6c46116b6ab1179b408c7aef9adb
nogvini <vinicius.nogueira@iesgo.edu.br>
2025-06-03 08:11:13 -0300

    refactor: remover função vazia analyzeDepositStatuses do ProfitCalculator para simplificar o código e melhorar a legibilidade - Ajuste que contribui para a manutenção futura do componente.

--- components/profit-calculator.tsx
+++ components/profit-calculator.tsx
@@ -3298,7 +3298,6 @@ export default function ProfitCalculator({
     }
   };
 
-  const analyzeDepositStatuses = () => {};
 
     return (
     <div className="w-full max-w-6xl mx-auto p-4 space-y-6">
